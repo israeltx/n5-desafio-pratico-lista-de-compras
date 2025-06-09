53b98533967b000ef794f6ea156ccd9963b208c5
Israel Teixeira <israeltx@gmail.com>
2025-06-09 14:50:00 -0300

    Add delete functionality

--- script.js
+++ script.js
@@ -2,50 +2,47 @@ const form = document.querySelector('form');
 const button = document.querySelector('button');
 const input = document.getElementById('new-item-input');
 const fieldset = document.querySelector('fieldset');
+const delete_btn = document.getElementsByClassName('delete');
 
-// Submit button functionality
 form.addEventListener('submit', (event) => {
   // Stop the submit button from refreshing the page
   event.preventDefault();
+
   // Get the value from the input
   const value = input.value;
 
+  // Create div and add the 'field' class
+  const div = document.createElement('div');
+  div.classList.add('field')
+
+  // If the input is not empty, create the item and add the value from input
   if (value != '') {
-    // Create elements
-    const div = document.createElement('div');
-    const label = document.createElement('label');
-    const checkbox = document.createElement('input');
-    const custom_checkbox_span = document.createElement('span');
-    const custom_checkbox_img = document.createElement('img');
-    const new_item_span = document.createElement('span');
-    const trash_can_img = document.createElement('img');
-
-    // Add classes to created elements
-    div.classList.add('field');
-    custom_checkbox_span.classList.add('custom-checkbox');
-    new_item_span.classList.add('item-text');
-
-    // Add sources to images
-    custom_checkbox_img.src = 'assets/icons/check-white.svg';
-    trash_can_img.src = 'assets/icons/trash-can.svg';
-
-    // Add type attribute to the input
-    checkbox.type = 'checkbox';
-    
-    // Append elements to the parents
-    custom_checkbox_span.appendChild(custom_checkbox_img);
-    label.appendChild(checkbox);
-    label.appendChild(custom_checkbox_span);
-    label.appendChild(new_item_span);
-    div.appendChild(label);
-    div.appendChild(trash_can_img);
-    fieldset.appendChild(div);
-
-    // Set the new item span content to the input value
-    new_item_span.innerText = value;
+    div.innerHTML = `
+      <label>
+        <input type="checkbox"/>
+        <span class="custom-checkbox">
+          <img src="assets/icons/check-white.svg" alt="">
+        </span>
+        <span class="item-text">${value}</span>
+      </label>   
+      <img src="assets/icons/trash-can.svg" alt="" class="delete">`;
+  
+    fieldset.appendChild(div)
 
     // Reset input after addition
     input.value = '';
   }
 });
 
+
+
+document.addEventListener('click', function (event) {
+  // Verifica se clicou em uma imagem com a classe 'delete'
+  const trashIcon = event.target.closest('.delete');
+  if (trashIcon) {
+    const fieldDiv = trashIcon.closest('.field');
+    if (fieldDiv) {
+      fieldDiv.remove();
+    }
+  }
+});
